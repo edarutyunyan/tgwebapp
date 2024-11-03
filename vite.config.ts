bd368fb83020ecbe2dd9587722e3b71f5cde3d9a
Eduard Arutyunyan <e.arutyunyan08@gmail.com>
2024-11-03 23:27:36 +0700

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "edarutyunyan.github.io/tgwebapp",
+  base: "edarutyunyan.github.io",
   build: {
     outDir: "build"
   }
