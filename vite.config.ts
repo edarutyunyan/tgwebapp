c07315a5399df973d436a48c20582422886d759a
Eduard Arutyunyan <e.arutyunyan08@gmail.com>
2024-11-09 14:11:03 +0700

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      /**
+       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+       * @default src/main.ts
+       */
+      entry: 'src/main.tsx',
+      /**
+       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+       * @default index.html
+       */
+      template: './index.html',
+    }),
+  ],
   base: "/tgwebapp",
   publicDir: './public',
   build: {
